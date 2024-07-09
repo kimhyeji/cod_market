f883f0f0940cbc6942b454a0aec887e26db7093d
kimhyeji <gpwl5589@gmail.com>
2024-07-09 15:11:23 +0900

    join service, entity, repository

--- src/test/java/com/cod/market/service/MemberServiceTests.java
+++ src/test/java/com/cod/market/service/MemberServiceTests.java
@@ -1,13 +1,13 @@
 package com.cod.market.service;
 
+import com.cod.market.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-public class MemberService {
+public class MemberServiceTests {
     
     @Autowired
     private MemberService memberService;
