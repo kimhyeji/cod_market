12c5344074071f184fbe1e75076e972e72f2b865
kimhyeji <gpwl5589@gmail.com>
2024-07-10 14:20:42 +0900

    detail data

--- src/main/java/com/cod/market/product/service/ProductService.java
+++ src/main/java/com/cod/market/product/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +25,11 @@ public class ProductService {
         p.setPrice(price);
         p.setCreateDate(LocalDateTime.now());
         productRepository.save(p);
+    }
+
+    public Product getProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        // TODO: 없을 경우 예외처리 예정
+        return product.get();
     }
 }
