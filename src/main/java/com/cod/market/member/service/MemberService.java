5265207be87ef7771abca571da3afcf03085121f
kimhyeji <gpwl5589@gmail.com>
2024-07-10 17:19:49 +0900

    question mvc complete

--- src/main/java/com/cod/market/member/service/MemberService.java
+++ src/main/java/com/cod/market/member/service/MemberService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +27,15 @@ public class MemberService {
         memberRepository.save(member);
 
         return member;
+    }
+
+    public Member findByUserName(String username) {
+        Optional<Member> member = memberRepository.findByUsername(username);
+
+        if ( member.isPresent() ) {
+            return member.get();
+        } else {
+            throw new RuntimeException("member not found");
+        }
     }
 }
