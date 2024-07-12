42bb1d5819b67e4914ab1123a508fedf85b2e2b5
kimhyeji <gpwl5589@gmail.com>
2024-07-12 15:47:57 +0900

    cart list

--- src/main/java/com/cod/market/cart/controller/CartController.java
+++ src/main/java/com/cod/market/cart/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.cod.market.cart.controller;
 
+import com.cod.market.cart.entity.Cart;
 import com.cod.market.cart.service.CartService;
 import com.cod.market.member.entity.Member;
 import com.cod.market.member.service.MemberService;
@@ -8,12 +9,14 @@ import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
@@ -25,7 +28,12 @@ public class CartController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/list")
-    public String list() {
+    public String list(Principal principal, Model model) {
+        Member member = memberService.findByUserName(principal.getName());
+        List<Cart> cartList = cartService.getList(member);
+
+        model.addAttribute("cartList", cartList);
+
         return "cart/list";
     }
 
