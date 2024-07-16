0e27a6645e4e86fff24cf195100b3a6daab2990f
kimhyeji <gpwl5589@gmail.com>
2024-07-16 14:12:22 +0900

    edit entity

--- src/main/java/com/cod/market/member/entity/Member.java
+++ src/main/java/com/cod/market/member/entity/Member.java
@@ -2,8 +2,11 @@ package com.cod.market.member.entity;
 
 import com.cod.market.base.BaseEntity;
 import com.cod.market.cart.entity.Cart;
+import com.cod.market.product.entity.Product;
 import com.cod.market.question.entity.Question;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -20,6 +23,9 @@ public class Member extends BaseEntity {
     private String nickname;
     private String email;
     private String isActive;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Product> ProductList;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<Question> questionList;
